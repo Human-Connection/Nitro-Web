2185f2d9fe7c16b953b87a08dbc2064d403e8de7
Lulalaby <lala@staff.latias.eu>
2019-03-13 20:58:52 +0100

    Discord integration frontend prepare

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express()
 
 require('dotenv').config()
 
-const host = process.env.HOST || '127.0.0.1'
+const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT || 3000
 app.set('port', port)
 
